d2068115ad09f6eacfb9e509d48237713fc58f58
MDMS29 <mdmazo30@minsena.edu.co>
2025-01-26 00:18:06 -0500

    clase 21 enero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { LoginGuard } from './guard/login.guard';
-import { IntroGuard } from './guard/intro.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule), canActivate: [LoginGuard, IntroGuard],
+    redirectTo: 'menu/home',
     pathMatch: 'full'
   },
   {
@@ -25,6 +18,10 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
   }
 ];
 
